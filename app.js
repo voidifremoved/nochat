96f6f842bcea3a5e7e971c943e4831da160f8706
Dave <dave@rubberjam.com>
2017-02-26 12:22:42 +0000

    Moressl

--- app.js
+++ app.js
@@ -215,15 +215,17 @@ function startApp() {
         http.createServer(redirectServer)
             .listen(settings.http.port || 5000, host);
 
+
+    }
+
+    app.listen(port, host);
+
 		app.get('*',function(req,res,next){
 		  if(req.headers['x-forwarded-proto']!='https')
 		    res.redirect('https://stardust1.herokuapp.com')
 		  else
 		    next() /* Continue to other routes if we're not redirecting */
 		})
-    }
-
-    app.listen(port, host);
 
     //
     // XMPP
