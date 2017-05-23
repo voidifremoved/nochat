d443a0fc8522ec16a3172f122bc6976bfda29a04
Dave <dave@rubberjam.com>
2017-05-23 20:36:20 +0100

    Fix again

--- app/controllers/presence.js
+++ app/controllers/presence.js
@@ -17,7 +17,6 @@ SocketIoConnection.prototype.disconnect = function() {
 
     this.socket.conn = null;
     this.socket = null;
-    window.location = '/logout';
 };
 
 module.exports = function() {
