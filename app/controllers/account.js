7c526d9eadb2a660b12dcfe8e2b216fe36017226
Dave <dave@rubberjam.com>
2017-07-03 20:13:46 +0100

    redirect fail

--- app/controllers/account.js
+++ app/controllers/account.js
@@ -24,17 +24,15 @@ module.exports = function() {
     //
     // Routes
     //
-    /*app.get('/', middlewares.requireLogin.redirect, function(req, res) {
+    app.get('/', middlewares.requireLogin.redirect, function(req, res) {
         res.render('chat.html', {
             account: req.user,
             settings: settings,
             version: psjon.version
         });
-    });*/
+    });
     
-    app.use('/', function(req,res,next){
-	  res.redirect('https://racing1.herokuapp.com/login');
-	});
+
     
     app.get('/', middlewares.requireLogin.redirect, function(req, res) {
         res.render('chat.html', {
